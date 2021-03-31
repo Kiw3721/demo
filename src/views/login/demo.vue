0013ce5f100bb8d5ece93d599ad9ad6ec4c003de
e <1125419193@qq.com>
2021-03-31 11:52:42 +0800

    3.31

--- src/views/login/demo.vue
+++ src/views/login/demo.vue
@@ -3,6 +3,18 @@
       <div class="title-container">
         <h3>欢迎{{ account }}进入该系统</h3>
       </div>
+  
+    <el-carousel indicator-position="none" arrow="always" :height="bannerHeight + 'px'">
+        <el-carousel-item v-for="(item, index) in dataimg" :key="index">
+          <el-row>
+            <el-col :span="24" class="banner_img">
+              <span>{{item.tet2}}</span>
+              <img ref="image" :src="item.src" alt="" @load="imageLoaded" class="img">
+            </el-col>
+          </el-row>
+        </el-carousel-item>
+    </el-carousel>
+
   </div>
 </template>
 
@@ -14,7 +26,20 @@ export default {
   name: "demo",
   data() {
     return {
-     
+      bannerHeight:"",
+     dataimg: [{
+						src: require('../../assets/images/lb1.jpg'),
+						txt2: '图1',
+					},
+					{
+						src: require('../../assets/images/lb2.jpg'),
+						txt2: '图2',
+					},
+					{
+						src: require('../../assets/images/lb3.jpg'),
+						txt2: '图3',
+					}
+				]
     };
   },
   computed: {
@@ -24,27 +49,54 @@ export default {
    
   },
   methods: {
-   
-  }
+   imageLoaded(){
+       this.$nextTick(()=>{
+         this.bannerHeight = this.$refs.image[0].height
+         console.log(this.$refs.image[0].height)
+       })
+     
+   }
+  },
+  mounted () {
+    this.imageLoaded();
+    window.addEventListener( 'resize', () => {
+      this.bannerHeight = this.$refs.image[0].height
+      this.imageLoaded();
+      }, false)
+      }, 
 };
 </script>
 
 <style lang="scss" scoped>
 .dashboard {
   &-container {
-    margin: 30px;
     position: relative;
-    width: 620px;
+    width: 100%px;
     max-width: 100%;
+    height: 100%;
     margin: 0 auto;
     overflow: hidden;
   }
-  &-text {
-    font-size: 25px;
-    line-height: 46px;
-    text-align: center;
-    margin: 10px 0;
-  }
 
 }
+h3{
+  font-size: 24px;
+  text-align: center;
+}
+
+.el-carousel__item span {
+    color: #475669;
+    font-size: 25px;
+    opacity: 0.75;
+    line-height: 30px;
+    margin: 0 auto;
+  }
+
+  .img{
+    display: inline-block; 
+    width: 100%; 
+    max-width: 100%; 
+    height: auto;
+  }
+
 </style>
